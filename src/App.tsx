82d159f6152a0a63a75caef7ae54ce5d8007c156
YehinnerTA <ytorresastorga@gmail.com>
2025-05-12 14:18:21 -0500

    Update Error Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,34 @@
 import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
+
+// Conexión de archivos intermediario
+import SplashLoader from './pages/SplashLoader';
+
+// Ubicación de vistas
+import SplashPage from './pages/SplashPage';
+import Login from './pages/Login';
+import Register from './pages/Register';
 import Home from './pages/Home';
 
-/* Estilos de Ionic */
+/* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
 import '@ionic/react/css/normalize.css';
 import '@ionic/react/css/structure.css';
 import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
 import '@ionic/react/css/padding.css';
 import '@ionic/react/css/float-elements.css';
 import '@ionic/react/css/text-alignment.css';
 import '@ionic/react/css/text-transformation.css';
 import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
+
+/* Theme variables */
 import './theme/variables.css';
 
 setupIonicReact();
@@ -22,7 +37,6 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-<<<<<<< HEAD
         {/* Ubicación */}
         <Route exact path="/" component={SplashLoader} />
         <Route path="/SplashPage" component={SplashPage} />
@@ -32,14 +46,6 @@ const App: React.FC = () => (
 
         {/* Redirección */}
         <Redirect to="/" />
-=======
-        <Route exact path="/home">
-          <Home />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
->>>>>>> 7c232684739b93deb60435db906c75670488807e
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
